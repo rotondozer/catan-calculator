7eefb122f1ffadfeeeee059184154fbe758218ab
Nick Rotondo <nickrotondo724@gmail.com>
2021-01-10 11:28:59 -0500

    refactor build_queue to be module, not class

--- src/build_queue.ts
+++ src/build_queue.ts
@@ -3,41 +3,34 @@ import * as BuildOption from "./build_option";
 
 const maybe = Maybe.fromNullable;
 
-type Queue = Array<BuildOption.BuildOption>;
+export type BuildQueue = Array<BuildOption.BuildOption>;
 
-export class BuildQueue {
-  queue: Queue;
+export function add(buildOpt: BuildOption.BuildOption, queue: BuildQueue): BuildQueue {
+  const queue2 = queue.length ? [ ...queue ] : [];
+  return maybe(queue2.pop()).caseOf({
+    Nothing: () => [buildOpt], // queue is empty
+    Just: lastOpt =>
+      queue2.concat(lastOpt.isSameOption(buildOpt) 
+        ? lastOpt.merge(buildOpt) 
+        : [lastOpt, buildOpt]), // make sure that last opt gets back in there
+  });
+}
 
-  constructor(fromQ?: Queue) {
-    this.queue = this.combineAdjacents(fromQ || []);
+export function combineAdjacents(queue: BuildQueue): BuildQueue {
+  if (queue.length === 0) {
+    return queue; // this is how the loop ends: when there aren't any elements ahead
   }
 
-  private combineAdjacents(queue_: Queue): Queue {
-    if (queue_.length === 0) {
-      return queue_; // this is how the loop ends: when there aren't any elements ahead
-    }
+  const currentOpt = queue[0];
 
-    const currentOpt = queue_[0];
-
-    return maybe(queue_[1]).caseOf({
-      Just: nextOpt => {
-        if (nextOpt.isSameOption(currentOpt)) {
-          return [currentOpt.merge(nextOpt), ...this.combineAdjacents(queue_.slice(2))];
-        } else {
-          return [currentOpt, ...this.combineAdjacents(queue_.slice(1))];
-        }
-      },
-      Nothing: () => [currentOpt],
-    });
-  }
-
-  public add(buildOpt: BuildOption.BuildOption): BuildQueue {
-    const updatedQueue = maybe(this.queue.pop()).caseOf({
-      Nothing: () => [buildOpt], // queue is empty
-      Just: lastOpt =>
-        this.queue.concat(lastOpt.isSameOption(buildOpt) ? lastOpt.merge(buildOpt) : [lastOpt, buildOpt]), // make sure that last opt gets back in there
-    });
-
-    return new BuildQueue(updatedQueue);
-  }
+  return maybe(queue[1]).caseOf({
+    Just: nextOpt => {
+      if (nextOpt.isSameOption(currentOpt)) {
+        return [currentOpt.merge(nextOpt), ...combineAdjacents(queue.slice(2))];
+      } else {
+        return [currentOpt, ...combineAdjacents(queue.slice(1))];
+      }
+    },
+    Nothing: () => [currentOpt],
+  });
 }
